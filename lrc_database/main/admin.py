456c85595b2592e72b5ed126d6c2ae2fcf0fa0d5
Simon Andrews <simon.andrews212@gmail.com>
2022-01-06 16:33:26 -0500

    Update admin UI, tweak models, add database bootstrapping script, remove migrations
     - Added script for creating a database and populating it with fake
     data. Use `manage.py bootstrapdatabase`.
     - Added dependency on Faker, a library for generating fake data, to aid
     with the bootstrapping script
     - Added proper admin interface setups
     - Removed migrations folder for now. It doesn't make sense to track
     this in git so early in development.
     - Added help text to some models
     - Allowed new data fields for tutoring shift model to be blank
     (sometimes you only want to update one thing at a time)
     - Removed some extra methods from the hardware model (will manage using
     Django's APIs)

--- lrc_database/main/admin.py
+++ lrc_database/main/admin.py
@@ -5,7 +5,7 @@ from .models import Course, Hardware, LRCDatabaseUser, TutoringShift, TutoringSh
 
 
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    ordering = ('department', 'number')
 
 
 class LRCDatabaseUserAdmin(UserAdmin):
@@ -13,15 +13,24 @@ class LRCDatabaseUserAdmin(UserAdmin):
 
 
 class TutoringShiftAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('tutor', 'start', 'duration', 'location')
 
 
 class TutoringShiftChangeRequestAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        ('Approval', {
+            'fields': ('target', 'reason', 'approved', 'approved_by', 'approved_on')
+        }),
+        ('New data', {
+            'fields': ('new_tutor', 'new_start', 'new_duration', 'new_location')
+        })
+    )
+    list_display = ('target', 'reason', 'approved', 'approved_by', 'approved_on')
 
 
 class HardwareAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'is_available')
+    ordering = ('name',)
 
 
 admin.site.register(Course, CourseAdmin)
--- lrc_database/main/models.py
+++ lrc_database/main/models.py
@@ -15,52 +15,40 @@ class Course(models.Model):
 class LRCDatabaseUser(AbstractUser):
     courses_tutored = models.ManyToManyField(Course)
 
+    def __str__(self):
+        if not (self.first_name and self.last_name):
+            return self.username
+        else:
+            return f'{self.first_name} {self.last_name}'
+
 
 class TutoringShift(models.Model):
-    tutor = models.ForeignKey(to=LRCDatabaseUser, on_delete=models.CASCADE)
-    start = models.DateTimeField()
-    duration = models.DurationField()
-    location = models.CharField(max_length=32)
+    tutor = models.ForeignKey(to=LRCDatabaseUser, on_delete=models.CASCADE, help_text='The tutor who is responsible for this session.')
+    start = models.DateTimeField(help_text='The time that the session starts.')
+    duration = models.DurationField(help_text='How long the session will last, in HH:MM:SS format.')
+    location = models.CharField(max_length=32, help_text='The location where the session will be held, e.g. GSMN 64.')
 
     def __str__(self):
         return f'{self.tutor} in {self.location} at {self.start}'
 
 
-class TutoringShiftChangeRequest(TutoringShift):
+class TutoringShiftChangeRequest(models.Model):
     target = models.ForeignKey(to=TutoringShift, related_name='tutoring_shift_change_request_target', on_delete=models.CASCADE, help_text='Tutoring shift to edit.')
     reason = models.CharField(max_length=512, help_text='Explanation for why this change is being requested.')
     approved = models.BooleanField(default=False, help_text='Whether the request is approved or not.')
-    approved_by = models.ForeignKey(to=LRCDatabaseUser, null=True, default=None, on_delete=models.CASCADE, help_text='The user (if any) who approved the change request.')
-    approved_on = models.DateTimeField(help_text='When the request was approved.')
+    approved_by = models.ForeignKey(to=LRCDatabaseUser, related_name='tutoring_shift_change_request_approved_by', blank=True, null=True, default=None, on_delete=models.CASCADE, help_text='The user (if any) who approved the change request.')
+    approved_on = models.DateTimeField(help_text='When the request was approved.', blank=True, null=True, default=None)
+
+    new_tutor = models.ForeignKey(to=LRCDatabaseUser, related_name='tutoring_shift_change_request_new_tutor', on_delete=models.CASCADE, null=True, default=None, help_text='The new tutor who will be responsible for the session if this request is approved.')
+    new_start = models.DateTimeField(blank=True, null=True, default=None, help_text='The new time that the session starts if this request is approved.')
+    new_duration = models.DurationField(blank=True, null=True, default=None, help_text='How long the session will last, in HH:MM:SS format, if this request is approved.')
+    new_location = models.CharField(max_length=32, blank=True, null=True, default=None, help_text='The new location where this session will be held, e.g. GSMN 64, if this request is approved.')
 
 
 class Hardware(models.Model):
 
     name = models.CharField(max_length=200)
-    isAvailable = models.BooleanField(True)
-
-    #inventory = models.ForeignKey('Inventory', on_delete=CASCADE)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name + " , " + self.product_ID
-
-    def checkAvailability(self):
-        return self.isAvailable
-
-    def changeAvailability(self):
-        if self.checkAvailability == True:
-            self.isAvailable = False
-        else:
-            self.isAvailable = True
-
-    def getName(self):
         return self.name
-
-    def changeName(self, newName):
-        self.name = newName
-
-    def getID(self):
-        return self.product_ID
-    
-    def changeID(self, newID):
-        self.product_ID = newID
