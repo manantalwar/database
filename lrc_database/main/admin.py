aa85b2ba78b941832f858f6cb9a19e0e6ef5cfd8
lrosenbaum4251 <lrosenbaum@umass.edu>
2022-01-11 19:17:26 -0500

    style

--- lrc_database/main/admin.py
+++ lrc_database/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import (Course, Hardware, Loan, LRCDatabaseUser, SISession, SISessionChangeRequest)
-from .models import (TutoringShift, TutoringShiftChangeRequest)
+from .models import (
+    Course, Hardware, Loan, LRCDatabaseUser, SISession,
+    SISessionChangeRequest, TutoringShift, TutoringShiftChangeRequest)
 
 
 class CourseAdmin(admin.ModelAdmin):
